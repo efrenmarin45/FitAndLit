537aef6acdb9e9302c9953a12ddad9ba017dab14
Efren_Marin <efren45marin@gmail.com>
2021-01-28 21:15:02 -0800

    Included first product on page. Need to optimize for mobile

--- src/components/SolutionsSection/SolutionsElements.js
+++ src/components/SolutionsSection/SolutionsElements.js
@@ -1,16 +1,60 @@
 import styled from 'styled-components';
 
 export const SolutionsContainer = styled.div`
-    width: auto;
-    background-color: pink;
+    width: 1800px;
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+`
+
+export const ProductTitle = styled.p`
+    background-color: #E89CED;
+    color: white;
+    text-shadow: 2px 2px 2px #9D0797;
+    font-family: 'Oswald', sans-serif;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 4em;
+    padding-top: 25px;
+
+    @media screen and (max-width: 1302px){
+        font-size: 3em;
+    }
+`
+
+export const SolutionButton = styled.button`
+    background-color: #99A4FF;
+    width: 300px;
+    height: 100px;
+    border: 5px solid white;
+    border-radius: 65px;
+    position: absolute;
+    top: 75%;
+    right: 10%;
+    color: white;
+    font-size: 2.5em;
+    font-weight: bold;
 `
 
 export const SolutionOne = styled.div`
-    background-color: red;
+    background-color: #E89CED;
+    height: 800px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+`
+
+export const SolutionTextOne = styled.p`
+    font-size: 3em;
+    background-color: #E89CED;
 `
 
 export const SolutionTwo = styled.div`
-    background-color: blue;
+    background-color: #FFF3FA;
+    height: 600px;
 `
 
 export const SolutionThree = styled.div`
--- src/components/SolutionsSection/index.js
+++ src/components/SolutionsSection/index.js
@@ -1,11 +1,17 @@
-import { SolutionsContainer, SolutionOne, SolutionTwo, SolutionThree, SolutionFour } from './SolutionsElements';
+import { SolutionsContainer, ProductTitle, SolutionOne, SolutionTwo, SolutionThree, SolutionFour, SolutionTextOne, SolutionButton } from './SolutionsElements';
+import Tea from '../../img/tea.png';
 
 const SolutionsSection = () => {
     return(
         <>
             <SolutionsContainer>
+                <ProductTitle>Iaso Tea</ProductTitle>
                 <SolutionOne>
-                test
+                    <img className='productOne' src={Tea} alt="Iaso Tea"></img>
+                    <SolutionTextOne>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu orci molestie, elementum sapien placerat, fringilla orci. Fusce id ipsum suscipit enim commodo commodo. Sed quis erat nibh. Sed egestas augue sed nisl tempus auctor. Fusce a metus vitae erat molestie gravida. 
+                    </SolutionTextOne>
+                    <SolutionButton>Learn More</SolutionButton>
                 </SolutionOne>
                 <SolutionTwo>
                 test

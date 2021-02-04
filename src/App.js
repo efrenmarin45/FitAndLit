10784a6394738907e460b48732a92eeba53326f0
Efren_Marin <efren45marin@gmail.com>
2021-02-03 21:50:03 -0800

    Minor bug fixes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react'; 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Pages'; 
 
 
 function App() {
     return(
-        <Router>
-            <Switch>
-                <Route path="/" exact component={Home} />
-            </Switch>
-        </Router>
+        <Home />
     );
 }
 
